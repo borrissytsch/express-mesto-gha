8a4eced90c5979fa2121e4fc6454baa91c9b8abf
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-02 05:58:50 +0300

    Preliminary version of Practicum 14th project (Sprint 14, Mesto 4 express gha, before 1st code review: Joi tests were added

--- middlewares/joiValidate.js
+++ middlewares/joiValidate.js
@@ -7,6 +7,7 @@ const signJoiTest = (signFields = {
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().uri(),
   }).unknown(true),
 }) => celebrate(signFields);
 const idJoiTest = (id = {
