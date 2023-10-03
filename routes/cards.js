59d1f997cd86d402a3e46d8b6495bf4af1955299
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-04 01:34:31 +0300

    Preliminary version of Practicum 14th project (Sprint 14, Mesto 4 express gha, before 1st code review: route paths corrected 4 id tests

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const cardRouter = require('express').Router();
 const { cardJoiTest, idJoiTest } = require('../middlewares/joiValidate');
-const { /* CARDS, */cardRoutes } = require('../utils/constants');
+const { CARDS, cardRoutes } = require('../utils/constants');
 const {
   getCards, createCard, deleteCardById, likeCard, dislikeCard,
 } = require('../controllers/cards');
@@ -11,8 +11,8 @@ cardRouter.get('/', getCards);
 // cardRouter.get(CARDS, getCards);
 cardRouter.post('/', cardJoiTest(), createCard);
 // cardRouter.post(CARDS, cardJoiTest(), createCard);
-cardRouter.delete(cardId, idJoiTest(), deleteCardById);
-cardRouter.put(cardLikes, idJoiTest(), likeCard);
-cardRouter.delete(cardLikes, idJoiTest(), dislikeCard);
+cardRouter.delete(`${CARDS}${cardId}`, idJoiTest(), deleteCardById);
+cardRouter.put(`${CARDS}${cardLikes}`, idJoiTest(), likeCard); // Joi requires abs paths
+cardRouter.delete(`${CARDS}${cardLikes}`, idJoiTest(), dislikeCard);
 
 module.exports = cardRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const userRouter = require('express').Router();
 // const { Joi, celebrate, Segments } = require('celebrate');
 const { idJoiTest, userJoiTest, avatarJoiTest } = require('../middlewares/joiValidate');
-const { /* USERS, */userRoutes } = require('../utils/constants');
+const { USERS, userRoutes } = require('../utils/constants');
 const {
   getUsers, getUserById, getUserIInfo, /* createUser, */updateProfile, updateAvatar,
 } = require('../controllers/users');
@@ -11,7 +11,7 @@ const { userId, userProfile, userAvatar } = userRoutes;
 userRouter.get('/', getUsers);
 // userRouter.get(USERS, getUsers);
 userRouter.get(userProfile, getUserIInfo);
-userRouter.get(userId, idJoiTest(), getUserById);
+userRouter.get(`${USERS}${userId}`, idJoiTest(), getUserById); // userId requires abs path 4 Joi
 // userRouter.get('/:userId', idJoiTest(), getUserById);
 userRouter.patch(userProfile, userJoiTest(), updateProfile);
 userRouter.patch(userAvatar, avatarJoiTest(), updateAvatar);
